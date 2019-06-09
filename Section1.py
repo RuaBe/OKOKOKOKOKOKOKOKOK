a2dc4ed53a6b925ff5b469636a68d16b498f5ac5
Kien Nguyen <smallturtlearkmobile@gmail.com>
2019-06-09 12:10:01 +0700

    .

---
+++ Section1.py
@@ -0,0 +1,14 @@
+h = float(input("nhap gia tri cua chieu cao tam giac :"))
+a = float(input("nhap gia tri do dai day cua tam giac :"))
+dienTichTamGiac = (h * a)*0.5
+print("Dien tich hinh tam giac =" ,dienTichTamGiac)
+
+print("----------------------------------------")
+
+r = float(input("nhap gia tri ban kinh"))                            
+dienTichHinhTron = 3.14*r*r
+print(dienTichHinhTron) 
+
+# float là tính số thập phân 
+# int là số nguyên 
+#  chuyển từ số 
